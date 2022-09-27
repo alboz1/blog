9f42f1baedead56e0ba480c23b657ab7623fb3c7
alboz1 <vucajjuerd@gmail.com>
2022-09-27 15:00:59 +0200

    fix bug: get tags only from published posts

--- backend/api/tags/tags.queries.js
+++ backend/api/tags/tags.queries.js
@@ -1,7 +1,9 @@
 const mysql = require('../../lib/mysql');
 
 async function get(columns, condition) {
-    const [ tags ] = condition ? await mysql().from('tags').where(condition).select(columns) : await mysql().from('tags').select(columns);
+    const [ tags ] = condition ?
+        await mysql().from('tags').where(condition).select(columns) :
+        await mysql().join('tags', 'blog_posts', 'tags.blog_id', 'blog_posts.id').where({['blog_posts.published']: true}).select(columns);
 
     return tags;
 }
